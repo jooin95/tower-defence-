4360a924eb85f2f8e86d147e52ac9a3fb813e524
jooin95 <jooin95@naver.com>
2019-05-23 19:30:30 +0900

    시작
    0523

---
+++ bullets.js
@@ -0,0 +1,37 @@
+var bullets = [];
+var bulletImage = new Image();
+bulletImage.src="image/bullet.png";
+function Bullet(x,y,target,hurt) {
+  this.x = x,
+  this.y = y,
+  this.target = target,
+  this.hurt = hurt
+};
+
+Bullet.prototype.r = rectWidth/4;
+Bullet.prototype.speed = baseSpeed*2;
+
+Bullet.prototype.move = function() {
+  //find unit vector
+  var xDist = this.target.x+rectWidth/2-this.x; //"+rectWidth/2" because we want bullet to go for center of enemy no top left corner
+  var yDist = this.target.y+rectWidth/2-this.y;
+  var dist = Math.sqrt(xDist*xDist+yDist*yDist);
+  this.x = this.x+this.speed*xDist/dist;
+  this.y = this.y+this.speed*yDist/dist;
+};
+
+Bullet.prototype.draw = function() {
+  context.beginPath();
+  context.drawImage(bulletImage,this.x,this.y,10, 10);
+};
+ 
+Bullet.prototype.checkCollision = function() {
+  if(this.x < this.target.x + rectWidth &&
+     this.x + this.r > this.target.x &&
+     this.y < this.target.y + rectWidth &&
+     this.y + this.r > this.target.y) {
+       this.target.life -= this.hurt;
+       return true;
+     }
+  return false;
+};
---
+++ towerUnits.js
@@ -0,0 +1,109 @@
+//defense towers
+// base tower 
+var towers=[];
+
+var tower1= new Image();
+tower1.src = ("image/tower1.png");
+var tower2= new Image();
+tower2.src = ("image/tower2.png");
+var tower3= new Image();
+tower3.src = ("image/tower3.png");
+
+//Tower 1
+function Tower(x,y,z) {
+  this.x = x,
+  this.y = y,
+  this.type=1
+}
+
+Tower.prototype.r = rectWidth; //radius
+//Tower.prototype.constructor = Tower1;
+Tower.prototype.type=1;
+Tower.prototype.rateOfFire = FPS; //DPS
+Tower.prototype.range = rectWidth*5;
+Tower.prototype.hurt = Enemy.prototype.maxLife/6; // m
+Tower.prototype.color = 'green';
+Tower.prototype.cost = 50;
+
+Tower.prototype.findTarget = function() {
+  //if no enemies, no target
+  if(enemies.length === 0) {
+    this.target = null;
+    return;
+  }
+  //if target dead, remove target reference
+  if(this.target && this.target.life <= 0) {
+    this.target = null;
+  }
+  //find first enemy withing range and select that as tower's target
+  for (var i = 0, j = enemies.length; i < j; i ++) {
+    var dist = (enemies[i].x-this.x)*(enemies[i].x-this.x+rectWidth)+(enemies[i].y-this.y)*(enemies[i].y-this.y+rectWidth); //rectWidth included to look at center of rectangle, not top left corner
+    if (dist < (this.range*this.range)) { //sqaure of range. avoice Math.sqrt which is expensive
+      this.target = enemies[i];
+      return; //only need a single target
+    }
+  }
+};
+
+Tower.prototype.findUnitVector = function() {
+  if (!this.target) return false; //if there is no target, dont bother calculating unit vector
+  var xDist = this.target.x-this.x;
+  var yDist = this.target.y-this.y;
+  var dist = Math.sqrt(xDist*xDist+yDist*yDist); 
+  this.xFire = this.x+this.r*xDist/dist; //where turret ends and bullets start
+  this.yFire = this.y+this.r*yDist/dist;
+};
+Tower.prototype.draw= function() {
+  //draw outter circle
+  if(this.type==1)
+     context.drawImage(tower1,this.x-25,this.y-25,50,50);
+  else if(this.type==3)
+     context.drawImage(tower3,this.x-25,this.y-25,50,50);
+  else if(this.type==2)
+     context.drawImage(tower2,this.x-25,this.y-25,50,50);
+};
+
+Tower.prototype.fire = function() {
+  this.rateOfFire--;
+  if(this.target && this.rateOfFire <=0) {
+    bullets.push(new Bullet(this.xFire,this.yFire,this.target,this.hurt));
+    //reset this objects rateOfFire to the prototypes
+    this.rateOfFire = this.constructor.prototype.rateOfFire;
+  };
+};
+
+//other types of towers
+//long range tower:
+
+var Tower2 = function(x,y,z) {
+  this.x = x,
+  this.y = y,
+  this.type=2
+}
+Tower2.prototype = Object.create(Tower.prototype);
+Tower2.prototype.constructor = Tower2;
+Tower2.prototype.type=2;
+Tower2.prototype.range = Tower.prototype.range*1.4;//looking to double area, not radius or range
+Tower2.prototype.color = 'brown';
+Tower2.prototype.cost = Tower.prototype.cost * 1.5;
+Tower2.prototype.rateOfFire = Tower.prototype.rateOfFire / 2;
+
+//short range high damage tower
+var Tower3 = function(x,y,z) {
+  this.x = x,
+  this.y = y,
+  this.type=3
+}
+Tower3.prototype = Object.create(Tower.prototype);
+Tower3.prototype.constructor = Tower3;
+Tower3.prototype.type=3;
+Tower3.prototype.range = Tower.prototype.range * 0.7; //0.7 rather than 0.5 because looking at area
+Tower3.prototype.hurt = Tower.prototype.hurt*2;
+Tower3.prototype.color = 'aqua';
+Tower3.prototype.cost = Tower.prototype.cost * 1.5;
+
+
+//populate array of towers
+//this is used to figure out which 
+//class of tower to add when mouse is clicked
+var towerClasses = [Tower,Tower2,Tower3];
